a43868b4cb4c8c2531253fdccb80967d9c88d07b
Vipul Hirani <hiranivipuls@gmail.com>
2025-05-14 22:28:05 +0530

    migration change

---
+++ src/database/migrations/20250512203502-create-expenses.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('expenses', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('gen_random_uuid()'),
+        primaryKey: true,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      expense_scope: {
+        type: Sequelize.ENUM('project', 'company'),
+        allowNull: false,
+      },
+      project_id: {
+        type: Sequelize.UUID,
+        allowNull: true,
+        references: {
+          model: 'projects',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      amount: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      created_by: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+
+    // Add a constraint to ensure project_id is not null when expense_scope is 'project'
+    await queryInterface.sequelize.query(`
+            ALTER TABLE expenses
+            ADD CONSTRAINT check_project_expense
+            CHECK (
+                (expense_scope = 'project' AND project_id IS NOT NULL) OR
+                (expense_scope = 'company' AND project_id IS NULL)
+            );
+        `);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('expenses');
+  },
+};
